6674108e8ea89cbcb85aa3db52cec7e06dbb3448
Kathleen Neumann <neumann@gbv.de>
2020-04-20 21:25:52 +0200

    update thesis editor forms

--- save/webpages/js/tiho.js
+++ save/webpages/js/tiho.js
@@ -1,6 +1,18 @@
 
 $(document).ready(function() {
 
+  if ($("input[name='genre']").val()  == 'dissertation'||'habilitation'||'diploma_thesis'||'master_thesis') {
+    if ($("input[name='/mycoreobject/metadata/def.modsContainer/modsContainer/mods:mods/mods:originInfo/mods:place/mods:placeTerm']").val()  == '') {
+      $("input[name='/mycoreobject/metadata/def.modsContainer/modsContainer/mods:mods/mods:originInfo/mods:place/mods:placeTerm']").val('Hannover');
+    };
+    if ($("input[name='/mycoreobject/metadata/def.modsContainer/modsContainer/mods:mods/mods:originInfo/mods:publisher']").val()  == '') {
+      $("input[name='/mycoreobject/metadata/def.modsContainer/modsContainer/mods:mods/mods:originInfo/mods:publisher']").val('Tierärztliche Hochschule Hannover');
+    };
+    if ($("select[name='/mycoreobject/metadata/def.modsContainer/modsContainer/mods:mods/mods:classification/@valueURIxEditor']").val()  == '') {
+      $("select[name='/mycoreobject/metadata/def.modsContainer/modsContainer/mods:mods/mods:classification/@valueURIxEditor']").val('bibliography');
+    };
+  }
+
   // spam protection for mails
   $('span.madress').each(function(i) {
       var text = $(this).text();
