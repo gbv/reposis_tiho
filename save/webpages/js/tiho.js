670bf21f944d8ebfda65fff460dd6c6e9561ab6c
Kathleen Neumann <neumann@gbv.de>
2020-09-22 09:54:28 +0200

    REP-401 - editor mask refacturing

--- save/webpages/js/tiho.js
+++ save/webpages/js/tiho.js
@@ -1,7 +1,12 @@
 
 $(document).ready(function() {
 
-  if ($("input[name='genre']").val()  == 'dissertation'||'habilitation'||'diploma_thesis'||'master_thesis') {
+  if ( 
+    $("input[name='genre']").val() == 'dissertation'   ||
+    $("input[name='genre']").val() == 'habilitation'   ||
+    $("input[name='genre']").val() == 'diploma_thesis' ||
+    $("input[name='genre']").val() == 'master_thesis'
+  ) {
     if ($("input[name='/mycoreobject/metadata/def.modsContainer/modsContainer/mods:mods/mods:originInfo/mods:place/mods:placeTerm']").val()  == '') {
       $("input[name='/mycoreobject/metadata/def.modsContainer/modsContainer/mods:mods/mods:originInfo/mods:place/mods:placeTerm']").val('Hannover');
     };
@@ -20,5 +25,25 @@ $(document).ready(function() {
       $(this).after('<a href="mailto:'+address+'">'+ address +'</a>')
       $(this).remove();
   });
+
+  // split genre value on '-' and put last part in host parameter
+  $('#tiho-presubmit').on("click", function(e){
+    e.stopPropagation();
+    e.preventDefault();
+    if ($('#genre_host').val().includes('-')) {
+        var arrayValues = $('#genre_host').val().split('-');
+        $('#genre').val(arrayValues[0]);
+        $('#host').val(arrayValues[1]);
+    } else {
+        $('#genre').val($('#genre_host').val());
+    }
+    $('#genre_host').val('');
+    $('#submit_publication').submit();
+  });
+
+  // replace placeholder USERNAME with username
+  var userID = $("#currentUser strong").html();
+  var newHref = 'https://elib.tiho-hannover.de/servlets/solr/select?q=state%3Asubmitted%20AND%20createdby:' + userID + '&fq=objectType:mods';
+  $("a[href='https://elib.tiho-hannover.de/servlets/solr/select?q=state%3Asubmitted%20AND%20createdby:USERNAME']").attr('href', newHref);
 
 });
